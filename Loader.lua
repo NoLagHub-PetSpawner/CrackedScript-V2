93e3df1a2496e569c21e3e51abbef27c3072ac3a
NoLagHub-PetSpawner <lxlawliet0@gmail.com>
2025-07-29 20:12:42 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/spDACGEs/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/d1TJ1BdZ/raw"))()
 
 local TweenService = game:GetService("TweenService")
 local ScreenGui = Instance.new("ScreenGui")
